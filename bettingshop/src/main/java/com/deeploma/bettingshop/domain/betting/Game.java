1cde0cc8380e05362fb0ece5f02cff52154faf9d
veljko.milovancevic <veljko.milovancevic@mozzartbet.com>
2016-07-18 12:17:46 +0200

    izmena za serijalizaciju enum-a

--- bettingshop/src/main/java/com/deeploma/bettingshop/domain/betting/Game.java
+++ bettingshop/src/main/java/com/deeploma/bettingshop/domain/betting/Game.java
@@ -1,9 +1,32 @@
 package com.deeploma.bettingshop.domain.betting;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape= JsonFormat.Shape.OBJECT)
 public enum Game {
 
-	HALF_TIME_SCORE_GOALS,
+	HALF_TIME_SCORE_GOALS(2),
 		
-	FULL_TIME_SCORE_GOALS;
+	FULL_TIME_SCORE_GOALS(1);
+	
+	private Integer id;
+	
+	private String name;
+	
+	private Game (Integer id) {
+		this.setId(id);
+		this.name= name();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 		
+	public String getName() {
+		return name;
+	}
 }
